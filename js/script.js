984057e9bb5cb6fc16d97fa1aace91f91b6cea45
Rania OS <raniaos2001@gmail.com>
2024-11-11 11:53:12 +0700

    push1

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const achievementList = localStorage.getItem("achievementList")
+  ? JSON.parse(localStorage.getItem("achievementList"))
+  : [];
+renderList();
+
+function addAchievement() {
+  console.log(document.querySelector("#inputAchievement").value);
+  let achievement = document.getElementById("inputAchievement").value;
+  const temp = {
+    id: achievementList.length + 1,
+    name: achievement,
+    isChecked: false,
+  };
+
+  achievementList.push(temp);
+
+  localStorage.setItem("achievementList", JSON.stringify(achievementList));
+
+  renderList();
+}
+
+function renderList() {
+  const achievementData = document.querySelector("#achievementList");
+  achievementData.innerHTML = "";
+  let row = "";
+  achievementList.forEach((value, index) => {
+    row += `
+    <div class="form-check">
+        <input
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckChecked"
+            ${value.isChecked ? "checked" : ""}
+            onclick="checkAchievement(${index})"
+        />
+        <label class="form-check-label" for="flexCheckChecked">
+            ${value.name}
+        </label>
+        <button class="btn btn-danger" onclick="deleteAchievement(${index})">X</button>
+        </div>
+    `;
+  });
+  achievementData.innerHTML = row;
+}
+
+function deleteAchievement(index) {
+  achievementList.splice(index, 1);
+  localStorage.setItem("achievementList", JSON.stringify(achievementList));
+  renderList();
+}
+
+function checkAchievement(index) {
+  achievementList[index].isChecked = !achievementList[index].isChecked;
+  localStorage.setItem("achievementList", JSON.stringify(achievementList));
+  renderList();
+}
